43182b9dfd37a2fa96c45af00b92d5785efc07ce
Tal Landsman <tallandsman00@gmail.com>
2022-03-22 23:31:44 +0800

    basic js functionaloty added

---
+++ homepage/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+$(window).on( 'load', () => {
+
+    //const switchButton = document.querySelector('.btn');
+    const switchButton = document.getElementById('dark-mode');
+
+    switchButton.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+
+        let currentClassName = document.body.className;
+
+        if(currentClassName == 'dark-mode'){
+            switchButton.innerHTML = "Make it light mode";
+        } else {
+            switchButton.innerHTML = "Make it dark mode";
+        }
+    });
+
+   
+
+});
